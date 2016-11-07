45baccfa21612bef84fe002b6904d0ddf51e7083
Illya Balabanov <ilyablbnv@gmail.com>
2016-11-07 16:55:57 +0100

    Basic html structure and layout with flexbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'main-app',
-    template: ''
+    template: require("./app.component.html"),
+    styles: [require("./app.component.css")]
 })
 export class AppComponent {
 }
