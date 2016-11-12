fe0b7ef17d8810522e89f08feb92f7c93ed05929
Illya Balabanov <ilyablbnv@gmail.com>
2016-11-12 14:08:37 +0100

    FormsModule and usage of two-data binding for 'name' and 'type'; parameter model class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { InputFormComponent } from "./components/input-form/input-form.component";
@@ -8,6 +9,7 @@ import { OutputFormComponent } from "./components/output-form/output-form.compon
 @NgModule({
     imports: [
         BrowserModule,
+        FormsModule
     ],
     declarations: [
         AppComponent,
--- src/app/components/input-form/input-form.component.ts
+++ src/app/components/input-form/input-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Parameter } from "../../shared/parameter.model";
 
 @Component({
     selector: 'input-form',
@@ -9,6 +10,10 @@ export class InputFormComponent {
     constructor() {
     }
 
+    // Defines a main types for user input form. New parameter could be 'string', 'integer' or 'boolean';
     types = ['String', 'Integer', 'Boolean'];
 
+    model = new Parameter('Delka', this.types[0]);
+
+    get diagnostic() { return JSON.stringify(this.model); }
 }
