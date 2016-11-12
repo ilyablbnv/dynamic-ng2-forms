83e19cbb5081c19fd0a17d37b48a2cf3c7de9ca6
Illya Balabanov <ilyablbnv@gmail.com>
2016-11-12 15:24:47 +0100

    Dynamically adding params field

--- src/app/components/input-form/input-form.component.ts
+++ src/app/components/input-form/input-form.component.ts
@@ -7,13 +7,18 @@ import { Parameter } from "../../shared/parameter.model";
     styles: [require("./input-form.component.css")]
 })
 export class InputFormComponent {
+    paramsList: Array<Parameter>;
+    types: Array<string> = ['String', 'Integer', 'Boolean'];
+    empty: boolean = false;
+
     constructor() {
+        this.paramsList = [];
     }
 
-    // Defines a main types for user input form. New parameter could be 'string', 'integer' or 'boolean';
-    types = ['String', 'Integer', 'Boolean'];
+    get diagnostic() { return JSON.stringify(this.paramsList); }
 
-    model = new Parameter('Delka', this.types[0]);
-
-    get diagnostic() { return JSON.stringify(this.model); }
+    addParametr() {
+        this.paramsList.push(new Parameter('', this.types[0])); // Set default type to a 'String'
+        this.empty = true;
+    }
 }
