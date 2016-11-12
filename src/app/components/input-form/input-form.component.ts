1e0d6ee0909bf969885e99160484086bbf6f8cb3
Illya Balabanov <ilyablbnv@gmail.com>
2016-11-12 19:42:25 +0100

    Resetting form btn; deleting params; fa-icons added

--- src/app/components/input-form/input-form.component.ts
+++ src/app/components/input-form/input-form.component.ts
@@ -9,16 +9,27 @@ import { Parameter } from "../../shared/parameter.model";
 export class InputFormComponent {
     paramsList: Array<Parameter>;
     types: Array<string> = ['String', 'Integer', 'Boolean'];
-    empty: boolean = false;
+    empty: boolean = true;
 
     constructor() {
         this.paramsList = [];
     }
 
-    get diagnostic() { return JSON.stringify(this.paramsList); }
+    get diagnostic() {
+        return JSON.stringify(this.paramsList);
+    }
 
-    addParametr() {
+    addParam() {
         this.paramsList.push(new Parameter('', this.types[0])); // Set default type to a 'String'
+        this.empty = false;
+    }
+
+    clearForm() {
+        this.paramsList.length = 0;
         this.empty = true;
+    }
+
+    deleteParam(index) {
+        this.paramsList.splice(index, 1);
     }
 }
